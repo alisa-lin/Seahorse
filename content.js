6ec821b8a54a320a55b4292882a691bfe38fa0d4
Caleb J Caldwell <ccaldw2@uwo.ca>
2019-09-14 12:47:26 -0400

    delegating functions to allow js injection of files dependent on which site is being accessed

--- content.js
+++ content.js
@@ -1,1 +1,16 @@
-// content.js
+import "github-tweak"
+import "linkedin-tweak"
+
+
+(function() {
+
+})
+
+
+function get_current_tab() {
+    return chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+    });
+}
+
+function delegate_tweaks(url) 

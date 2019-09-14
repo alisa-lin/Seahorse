afada407fa917e478c138fa515d05a2c7516097e
Caleb J Caldwell <ccaldw2@uwo.ca>
2019-09-14 17:09:02 -0400

    sth

--- content.js
+++ content.js
@@ -1,16 +1,35 @@
-import "github-tweak"
-import "linkedin-tweak"
+// import "github-tweak"
+// import "linkedin-tweak"
 
 
 (function() {
-
+    document.addEventListener("DOMContentLoaded", format_page)
+    console.log("here")
 })
 
 
-function get_current_tab() {
-    return chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-        var url = tabs[0].url;
-    });
+function format_page() {
+    console.log("ayy")
 }
 
-function delegate_tweaks(url) 
+// function get_current_tab() {
+//     return chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+//         return tabs[0].url;
+//     });
+// }
+
+// function delegate_tweaks(url) {
+//     if (url.includes("github")){
+
+//     }
+
+//     else if (url.includes("linkedin")){
+
+//     }
+
+//     else {
+//         break
+//     }
+// }
+
+// function inject_js()

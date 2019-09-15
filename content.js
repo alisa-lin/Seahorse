32c3753fc606382259a20c9a54411a81eb3cc033
alisa-lin <alisalin610@gmail.com>
2019-09-15 00:59:05 -0400

    delete unused code

--- content.js
+++ content.js
@@ -1,37 +1,3 @@
-// import "github-tweak"
-// import "linkedin-tweak"
-
-(function() {
-  document.addEventListener("DOMContentLoaded", format_page);
-  console.log("here");
-});
-
-function format_page() {
-  console.log("ayy");
-}
-
-// function get_current_tab() {
-//     return chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-//         return tabs[0].url;
-//     });
-// }
-
-// function delegate_tweaks(url) {
-//     if (url.includes("github")){
-
-//     }
-
-//     else if (url.includes("linkedin")){
-
-//     }
-
-//     else {
-//         break
-//     }
-// }
-
-// function inject_js()
-
 // Replace images
 
 // Seahorse object to construct replacement images

3eaf7a140d87e92a87a4f73b8eb11eee5fd054de
Lynn <lynnkim2000@gmail.com>
2019-09-15 00:40:29 -0400

    text replacement

--- content.js
+++ content.js
@@ -1,15 +1,13 @@
 // import "github-tweak"
 // import "linkedin-tweak"
 
-
 (function() {
-    document.addEventListener("DOMContentLoaded", format_page)
-    console.log("here")
-})
-
+  document.addEventListener("DOMContentLoaded", format_page);
+  console.log("here");
+});
 
 function format_page() {
-    console.log("ayy")
+  console.log("ayy");
 }
 
 // function get_current_tab() {
@@ -38,66 +36,92 @@ function format_page() {
 
 // Seahorse object to construct replacement images
 function Seahorse(ratio, imageurl) {
-	this.ratio = ratio;
-	this.imageurl = imageurl;
+  this.ratio = ratio;
+  this.imageurl = imageurl;
 }
 
 // Determine image ratio
 function imageRatio(image) {
-	var proportion = image.height/image.width;
+  var proportion = image.height / image.width;
 
-	if (proportion > 1) {
-		return "vertical";
-	} else if (proportion === 1) {
-		return "square";
-	} else if (proportion < 1) {
-		return "horizontal"
-	}
+  if (proportion > 1) {
+    return "vertical";
+  } else if (proportion === 1) {
+    return "square";
+  } else if (proportion < 1) {
+    return "horizontal";
+  }
 }
 
 // library of replacement images
 var imageLibrary = [
-new Seahorse("vertical", "https://i.imgur.com/CkosoIh.png"),
-new Seahorse("square", "https://i.imgur.com/GBL6nAa.png"),
-new Seahorse("horizontal", "https://i.imgur.com/PqrrQlZ.png")
-]
+  new Seahorse("vertical", "https://i.imgur.com/CkosoIh.png"),
+  new Seahorse("square", "https://i.imgur.com/GBL6nAa.png"),
+  new Seahorse("horizontal", "https://i.imgur.com/PqrrQlZ.png")
+];
 
 // get the correct image for the image ratio
 var getSeahorse = {
-	init: function(imageLibrary) {
-		this.imageLibrary = imageLibrary;
-	},
+  init: function(imageLibrary) {
+    this.imageLibrary = imageLibrary;
+  },
 
-	vertical: function() {
-		return this.imageLibrary[0];
-	},
+  vertical: function() {
+    return this.imageLibrary[0];
+  },
 
-	square: function() {
-		return this.imageLibrary[1];
-	},
+  square: function() {
+    return this.imageLibrary[1];
+  },
 
-	horizontal: function() {
-		console.log("horizontal image found!")
-		return this.imageLibrary[2];
-	}
+  horizontal: function() {
+    console.log("horizontal image found!");
+    return this.imageLibrary[2];
+  }
 };
 
-function Randomize(images){
-    return Math.floor(Math.random() * images.length)
- }
+function Randomize(images) {
+  return Math.floor(Math.random() * images.length);
+}
 
 // the image replacement magic
-(function (document) {
-	getSeahorse.init(imageLibrary);
+(function(document) {
+  getSeahorse.init(imageLibrary);
 
-	// find what to replace on the page
-	var images = document.getElementsByTagName('img'), length = images.length;
+  // find what to replace on the page
+  var images = document.getElementsByTagName("img"),
+    length = images.length;
 
-	// replace images
-	for (var i = 0; i < length; i++) {
-		var ratio = imageRatio(images[i]);
-		console.log(getSeahorse[ratio]())	
-		var img = getSeahorse[ratio]();	
-		images[i].src = img.imageurl;
-	}
+  // replace images
+  for (var i = 0; i < length; i++) {
+    var ratio = imageRatio(images[i]);
+    console.log(getSeahorse[ratio]());
+    var img = getSeahorse[ratio]();
+    images[i].src = img.imageurl;
+  }
 })(document);
+
+// array of everything
+var elementsInsideBody = [...document.body.getElementsByTagName("*")];
+
+// loop
+function findAndReplace() {
+  elementsInsideBody.forEach(element => {
+    element.childNodes.forEach(child => {
+      if (child.nodeType === 3) {
+        replaceText(child);
+      }
+    });
+  });
+}
+
+function replaceText(node) {
+  let value = node.nodeValue;
+  value = value.replace(/She /g, "He/She  ");
+  value = value.replace(/He /g, "He/She ");
+  value = value.replace(/she /g, "he/she ");
+  value = value.replace(/he /g, "he/she  ");
+  node.nodeValue = value;
+}
+
+window.onload = findAndReplace();
